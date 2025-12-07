59641fd50ee51d60dfa29a034af7673dab9f8812
Navneet Ranjan <150068706+navneetnr@users.noreply.github.com>
2025-12-07 19:15:34 +0530

    Implement data cleaning for missing values
    Added data cleaning steps to handle missing values.

--- code.py
+++ code.py
@@ -3,8 +3,16 @@ import pandas as pd
 #load dataset
 df=pd.read_csv("C:\\Users\\LENOVO\\Desktop\\Sem-5\\ml ca2\\Metabolic_Syndrome.csv")
 print(df)
-print(df.info())
+df.info()
 print(df.describe())
 print(df.shape)
 print(df.isnull().sum())
-
+#data cleaning
+df["Marital"].fillna(df["Marital"].mode()[0], inplace=True)
+print(df.isnull().sum())
+df["Income"].fillna(df["Income"].median(),inplace=True)
+print(df.isnull().sum())
+df["WaistCirc"].fillna(df["WaistCirc"].median(),inplace=True)
+print(df.isnull().sum())
+df["BMI"].fillna(df["BMI"].median(),inplace=True)
+print(df.isnull().sum())
