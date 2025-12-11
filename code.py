fabaf36910eb4cb51581b3e1560cd7d4be53c99e
Navneet Ranjan <150068706+navneetnr@users.noreply.github.com>
2025-12-12 00:09:50 +0530

    Implement data preprocessing and logistic regression
    Added data preprocessing, scaling, and logistic regression for model training and evaluation.

--- code.py
+++ code.py
@@ -34,3 +34,37 @@ le=LabelEncoder()
 df["Marital"]=le.fit_transform(df["Marital"])
 print(df)
 
+
+x=df.drop("MetabolicSyndrome",axis=1)
+y=df["MetabolicSyndrome"]
+
+print(df["MetabolicSyndrome"].value_counts())
+#balance dataset
+
+ru=RandomUnderSampler()
+ru_x,ru_y=ru.fit_resample(x,y)
+print(ru_y.value_counts())
+
+x_train,x_test,y_train,y_test=train_test_split(ru_x,ru_y,test_size=0.2,random_state=42)
+
+#Standered Scaler
+num_cols = ["Age", "Income", "WaistCirc", "BMI", "HDL", "Triglycerides"] 
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(x_train[num_cols])
+x_test_scaled = scaler.transform(x_test[num_cols])
+
+x_train[num_cols] = x_train_scaled
+x_test[num_cols] = x_test_scaled
+
+
+
+#using logistic regression
+lr=LogisticRegression(max_iter=5000, solver='saga',C=1.0)
+lr.fit(x_train,y_train)
+acc=lr.score(x_test,y_test)*100
+print(f"Test Accuracy: {acc:.2f}%")
+
+sns.heatmap(x_train.corr(), annot=True, fmt=".2f", cmap="coolwarm")
+plt.show()
+
+
